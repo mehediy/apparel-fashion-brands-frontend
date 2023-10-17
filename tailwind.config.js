9d99d8cc8d127cd13203137e53cf66dc3a19da12
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-10-18 02:04:04 +0600

    font & color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      gray: {
+        1: "#F0F0F0",
+        2: "##D8D8D8",
+        3: "#333333",
+      },
+      white: "#FFF",
+      dark: "#232E21",
+      primary: "##FF0073",
+      secondary: {
+        1: "#FFF2F9",
+        2: "##F4C3C2",
+        3: "##F9DC5C",
+      },
+      accent: {
+        1: "##3772FF",
+      },
+    },
     extend: {},
   },
   plugins: [],
