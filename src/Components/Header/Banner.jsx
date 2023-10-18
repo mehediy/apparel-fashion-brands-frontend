678cf240886cc4a8af286b0b88f798f5a127c8f4
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-10-18 13:40:34 +0600

    added banner

---
+++ src/Components/Header/Banner.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import banner1 from "./../../assets/banner-1.jpg";
+import banner2 from "./../../assets/banner-2.webp";
+import banner3 from "./../../assets/aarong-1.webp";
+import banner4 from "./../../assets/aarong-2.webp";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./Banner.css";
+
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Link } from "react-router-dom";
+
+const Banner = () => {
+  return (
+    <>
+      <div className="max-w-[1920px] mx-auto h-[600px] 2xl:h-[800px]">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <Link to={"/lereve"}>
+              <img src={banner1} />
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to={"/bata"}>
+              <img src={banner2} />
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to={"/aarong"}>
+              <img src={banner3} />
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to={"/aarong"}>
+              <img src={banner4} />
+            </Link>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
