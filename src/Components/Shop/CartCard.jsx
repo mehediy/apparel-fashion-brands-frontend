8909f632175d8e749caad04204d289a0c4849307
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-10-20 19:49:05 +0600

    delete cart item

--- src/Components/Shop/CartCard.jsx
+++ src/Components/Shop/CartCard.jsx
@@ -1,9 +1,24 @@
 import { Rating } from "@smastrom/react-rating";
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
+import { toast } from "react-toastify";
 
 const CartCard = ({ data }) => {
   const { _id, brandId, name, brand, type, image, price, rating } = data;
+  const { user } = useContext(AuthContext);
+  const deleteHandler = (_id) => {
+    fetch(`http://localhost:5000/user/${user.email}/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount == 1) {
+          toast.success("Deleted!");
+        }
+      });
+  };
   return (
     <div className="h-[200px] overflow-hidden relative shadow-md hover:shadow-lg transition duration-150 hover:ease-in-out flex gap-6 p-4">
       <div className="overflow-hidden rounded">
@@ -28,16 +43,16 @@ const CartCard = ({ data }) => {
         <div className="flex justify-between gap-2 text-lg py-2 mt-2 w-full">
           <Link
             to={`/brand/${brandId}/${_id}`}
-            className="bg-gray-1 hover:bg-primary hover:text-white px-2 py-1 rounded outline outline-1 outline-gray-2 hover:outline-primary transition duration-150 hover:ease-in-out w-full text-center"
+            className="bg-gray-1 hover:bg-accent-1 hover:text-white px-2 py-1 rounded outline outline-1 outline-gray-2 hover:outline-accent-1 transition duration-150 hover:ease-in-out w-full text-center"
           >
             Details
           </Link>
-          <Link
-            to={`/product/update/${_id}`}
-            className="bg-white hover:bg-accent-1 hover:text-white px-2 py-1 rounded outline outline-1 outline-gray-2 hover:outline-accent-1 transition duration-150 hover:ease-in-out w-full text-center"
+          <button
+            onClick={() => deleteHandler(_id)}
+            className="bg-primary text-white hover:bg-gray-1 hover:text-dark px-2 py-1 rounded outline outline-1 outline-gray-2 hover:outline-gray-2 transition duration-150 hover:ease-in-out w-full text-center"
           >
-            Update
-          </Link>
+            Delete
+          </button>
         </div>
       </div>
     </div>
