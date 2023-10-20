48829f5be9ad1ce99af5276a9323591d40c34907
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-10-20 19:09:20 +0600

    added cart card

--- src/Components/Shop/CartCard.jsx
+++ src/Components/Shop/CartCard.jsx
@@ -1,7 +1,47 @@
+import { Rating } from "@smastrom/react-rating";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CartCard = ({ data }) => {
-  return <div>{data.name}</div>;
+  const { _id, brandId, name, brand, type, image, price, rating } = data;
+  return (
+    <div className="h-[200px] overflow-hidden relative shadow-md hover:shadow-lg transition duration-150 hover:ease-in-out flex gap-6 p-4">
+      <div className="overflow-hidden rounded">
+        <div className="w-[250px] rounded flex items-center justify-center overflow-hidden">
+          <img className="w-full" src={image} alt={name} />
+        </div>
+        <div className="absolute top-4 left-4 text-base text-white bg-accent-1 px-2 py-1 rounded-md">
+          <span>{brand}</span>
+        </div>
+      </div>
+      <div>
+        <div className="flex flex-col w-full gap-1 text-base">
+          <span className="text-sm text-gray-3">{type}</span>
+          <Link to={`/brand/${brandId}/${_id}`}>
+            <h2 className="font-medium text-lg">{name}</h2>
+          </Link>
+
+          <span>Price: {price} Tk</span>
+          <Rating style={{ maxWidth: 120 }} value={rating} readOnly />
+        </div>
+
+        <div className="flex justify-between gap-2 text-lg py-2 mt-2 w-full">
+          <Link
+            to={`/brand/${brandId}/${_id}`}
+            className="bg-gray-1 hover:bg-primary hover:text-white px-2 py-1 rounded outline outline-1 outline-gray-2 hover:outline-primary transition duration-150 hover:ease-in-out w-full text-center"
+          >
+            Details
+          </Link>
+          <Link
+            to={`/product/update/${_id}`}
+            className="bg-white hover:bg-accent-1 hover:text-white px-2 py-1 rounded outline outline-1 outline-gray-2 hover:outline-accent-1 transition duration-150 hover:ease-in-out w-full text-center"
+          >
+            Update
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartCard;
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -14,35 +14,43 @@ const Cart = () => {
       .then((res) => res.json())
       .then((data) => {
         setCartItems(data.cart);
-
-        fetch(`http://localhost:5000/cart-products`, {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({ cartItems }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            setCartProducts(data);
-            console.log(data);
-            setLoading(false);
-          });
+        setLoading(false);
       });
   }, []);
 
-  console.log(cartProducts);
+  useEffect(() => {
+    if (!loading) {
+      fetch(`http://localhost:5000/cart-products`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ cartItems }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setCartProducts(data);
+        });
+    }
+  }, [cartItems, loading]);
+
+  // console.log(cartProducts);
   // console.log(cartItems);
 
   return (
     <div className="container mx-auto px-4 py-4">
-      <h1 className="text-3xl">Your Cart</h1>
-      {loading ? (
+      <h1 className="text-3xl lg:text-5xl text-center font-medium py-8">
+        My Cart
+      </h1>
+
+      {loading == true ? (
         <Loading />
       ) : (
-        cartProducts.map((product) => (
-          <CartCard key={product._id} data={product} />
-        ))
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {cartProducts.map((product) => (
+            <CartCard key={product._id} data={product} />
+          ))}
+        </div>
       )}
     </div>
   );
